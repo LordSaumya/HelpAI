2af2e6a302511b99f24e8d66ee02dee8ffd35f3a
Zhen Yang <zzhenyyang@gmail.com>
2024-01-20 14:17:21 +0800

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,39 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Textarea } from '@chakra-ui/react'
+import React from 'react'
+
 export default function App() {
+  const [value, setValue] = React.useState('')
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  const handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
+
   return (
-    <h1 className="text-8xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className='min-h-screen p-28 flex flex-col'>
+        <Textarea
+          value={value}
+          onChange={handleInputChange}
+          placeholder='Here is a sample placeholder'
+          size='sm'
+          resize={'none'}
+          
+        />
+        <Button colorScheme='red' size='lg' isLoading={isLoading} padding={10} margin={10} marginX={20}>
+          Start
+        </Button>
+        <Textarea
+          value={value}
+          onChange={handleInputChange}
+          placeholder='Here is a sample placeholder'
+          size='sm'
+          resize={'none'}
+          height={200}
+        />
+      </div>
+    </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const theme = extendTheme({
+  config :{
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
